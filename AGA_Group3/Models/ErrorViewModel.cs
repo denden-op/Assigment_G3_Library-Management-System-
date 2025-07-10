3fa5abc93d402c48b3e9847b927701348cad4c6b
Group3_AGA <souno@DESKTOP-JCMA66N>
2025-07-10 08:51:43 +0700

    oudom_test

--- AGA_Group3/Models/ErrorViewModel.cs
+++ AGA_Group3/Models/ErrorViewModel.cs
@@ -3,7 +3,7 @@ namespace AGA_Group3.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
+        
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }

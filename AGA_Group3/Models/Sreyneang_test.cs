dd977d113023da8b59d31ee7cdb8bff2987ee3a2
Group3_AGA <souno@DESKTOP-JCMA66N>
2025-07-10 09:27:33 +0700

    test

---
+++ AGA_Group3/Models/Sreyneang_test.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Security;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AGA_Group3.Models
+{
+    public class Sreyneang_test : Controller
+    {
+        public IActionResult Index()
+        {
+           "hello teacher"
+            return View();
+        }
+    }
+}

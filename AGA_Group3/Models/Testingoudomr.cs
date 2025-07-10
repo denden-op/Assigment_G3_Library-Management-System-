ac70a99491b1dbf5503007b9e5fe35f750169d17
Group3_AGA <souno@DESKTOP-JCMA66N>
2025-07-10 08:58:42 +0700

    testing_dom

---
+++ AGA_Group3/Models/Testingoudomr.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AGA_Group3.Models
+{
+    public class Testingoudomr : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

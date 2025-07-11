03fc30de765bda95a027f0865bda79bac2c50989
Group3_AGA <souno@DESKTOP-JCMA66N>
2025-07-11 19:44:48 +0700

    testing

---
+++ AGA_Group3/Views/Sounoudom.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AGA_Group3.Views
+{
+    public class Sounoudom : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
